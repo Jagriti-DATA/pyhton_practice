215825cc6d1ea5c8f9ce31f6dd1029f0d173b39f
Jagriti-DATA <jagritikumari0459@gmail.com>
2025-05-14 18:47:25 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,33 @@
+#make calculator using elif
+print("what are you want to perform")
+print("""1. PRESS 1 FRO ADDITION
+"2. PRESS 2 FOR  SUBSTRACTION 
+"3. PRESS 3 FOR MULTIPLICATION
+"4. PRESS 4 FOR DIVISION""")
+
+choice = int(input("Enter  your choice "))
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == 1:
+    sum = num1 + num2
+    print("sum will be:", sum)
+
+elif choice == 2:
+    sub = num1 - num2
+    print("substraction will be:", sub)
+
+elif choice == 3:
+    mul = num1 * num2
+    print("multiplication will be:", mul)
+
+elif choice == 4:
+    if num2 != 0:
+        div = num1 / num2
+        print("division will be:", div)
+    else:
+        print("Error: number Cannot divide by zero!")
+
+else:
+    print("Invalid input. Please enter 1, 2, 3, or 4.")
